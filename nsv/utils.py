28c8ae50a3c0e6a626b4c2699afdfee6c197baab
Mattia Almansi <mattia.almansi@noc.ac.uk>
2022-02-03 19:33:51 +0000

    review Q2018

--- nsv/utils.py
+++ nsv/utils.py
@@ -1,14 +1,43 @@
+from functools import wraps
+
 import cf_xarray  # noqa: F401
 from gsw import SA_from_SP, pt0_from_t
 from xarray import Dataset
 
 
-def add_cf_attributes(ds: Dataset) -> Dataset:
-    coords = ["longitude", "latitude", "distance"]
-    ds = ds.set_coords(
-        [var for var in ds.data_vars for coord in coords if coord in var]
-    )
-    return ds.cf.guess_coord_axis().cf.add_canonical_attributes()
+def final_cleanup_before_returning(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        ds = func(*args, **kwargs)
+
+        # Set coordinates
+        coords = ["longitude", "latitude", "distance", "time"]
+        ds = ds.set_coords(
+            [var for var in ds.data_vars for coord in coords if coord in var]
+        )
+        ds = ds.cf.guess_coord_axis()
+
+        # Add cf attributes
+        ds = ds.cf.add_canonical_attributes()
+
+        # Sort stations by longitute
+        ds = ds.cf.sortby("longitude")
+        if "time" in ds:
+            ds = ds.sortby("time")
+
+        # Don't use strings for stations
+        if "station" in ds.variables:
+            ds["station_id"] = ds["station"].astype(str)
+            ds = ds.drop("station")
+
+        # Attributes
+        ds.attrs[
+            "description"
+        ] = f"Standardized {func.__name__.upper()}{args[1:] or ''}{kwargs or ''}"
+        ds.attrs["featureType"] = "timeSeries"
+        return ds
+
+    return wrapper
 
 
 def add_attributes_and_rename_variables(ds: Dataset, attrs_dict: dict) -> Dataset:
